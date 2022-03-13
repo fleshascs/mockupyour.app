b47bdbbdeeea93ab22b8981736b855118deb0cf2
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-13 13:34:56 +0200

    fix github pages asset paths

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     disableStaticImages: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/mockupyour.app' : ''
+  basePath: process.env.NODE_ENV == 'production' ? '/mockupyour.app' : ''
 };
 
 module.exports = withPlugins(
