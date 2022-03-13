5155c54c4025cf49b027a204f104eaa98648ab85
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-13 13:46:35 +0200

    add threejs

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteName: 'mockupyour.app',
-  domain: 'https://mockupyour.app',
+  domain: 'https://fleshascs.github.io/mockupyour.app',
   defaultTitle: 'mockup app',
   defaultDescription: 'description...',
   googleAnalyticsKey: 'UA-107670880-1'
