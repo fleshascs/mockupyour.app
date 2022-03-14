6f0f31d2d769bba732e9629672452e3503a08c6e
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-14 16:28:49 +0200

    ts fix

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,4 +1,6 @@
-export function Tag({ children }) {
+import { ReactNode } from 'react';
+
+export function Tag({ children }: { children: ReactNode }) {
   return (
     <div className='flex justify-end'>
       <h2 className='text-4xl rounded-l-xl border-l-4 border-l-rose-900 bg-[#9d00004d] p-2'>
