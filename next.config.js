491d7a48fd99bec7b5dd2295fc678261da5bce3a
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-14 17:03:23 +0200

    fix asset paths

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     disableStaticImages: true
   },
   // basePath: '/mockupyour.app'
-  exportAssetPrefix: process.env.GH_PAGES ? '/mockupyour.app' : ''
+  assetPrefix: process.env.GH_PAGES ? '/mockupyour.app' : ''
   // basePath: process.env.GH_PAGES ? '/mockupyour.app' : ''
   // basePath: process.env.NODE_ENV == 'production' ? '/mockupyour.app' : ''
 };
