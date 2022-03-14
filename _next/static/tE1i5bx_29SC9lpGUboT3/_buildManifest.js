cf225a428653ff88d0b3f414357cc922a5c45aaf
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-14 14:33:10 +0000

    Deploying to gh-pages2 from @ fleshascs/mockupyour.app@efb28a7af298c3878306310ff6980392a0d2ce52 🚀

---
+++ _next/static/tE1i5bx_29SC9lpGUboT3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c4918c90b5d965e0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/cs-1.6-csgo-mod":["static/chunks/pages/cs-1.6-csgo-mod-17dc47dcef3e9151.js"],sortedPages:["/","/_app","/_error","/cs-1.6-csgo-mod"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
